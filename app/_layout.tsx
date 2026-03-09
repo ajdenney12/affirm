0679e5f8a349ffc8ea8129a2aa1b22107b21f064
ajdenney12 <ajdenney12@gmail.com>
2026-03-08 22:25:40 -0400

    deleted layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar'
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   useFrameworkReady();
