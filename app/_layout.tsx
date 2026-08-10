1cc0f3ba0885c5faf0394b5972f92df4c5ef3da3
Ashley Denney <ashleydenney@ashs-neo.mynetworksettings.com>
2026-08-09 21:50:05 -0400

    Fix Expo Router paths

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,15 +4,15 @@ import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   useFrameworkReady();
+
   return (
     <>
       <StatusBar style="auto" />
+
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(auth)/login" />
+        <Stack.Screen name="auth/login" />
         <Stack.Screen name="(tabs)" />
       </Stack>
     </>
   );
 }
-
